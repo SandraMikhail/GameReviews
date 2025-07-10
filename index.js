417073f3be1ace2c423f6cf305d6662863c17a10
sandraMikhail <sandra_samir@live.com>
2025-07-10 12:59:28 +0200

    loading screen, navbar and some oop added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+import {Ui} from './ui.js'
+
 const gameData = document.getElementById("gameData");
+const modalbody = document.querySelector('.modal-body');
+const ui = new Ui(gameData,modalbody);
+let games = [];
+let loading = document.getElementById("loading")
+
 let clickedValue=""
 
  const options = {
@@ -9,12 +16,13 @@ let clickedValue=""
     },
   };
 
-let gameDetail ;
+let gameDetail;
+
 
 async function getGames(q = "shooter") {
- 
-
   try {
+    loading.classList.remove('d-none')
+
     let response = await fetch(
       `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${q}  `,
       options
@@ -22,9 +30,13 @@ async function getGames(q = "shooter") {
     let data = await response.json();
     console.log(data);
     games = data;
-    displayGame();
+    ui.displayGame(games);
+
   } catch (error) {
+  
     console.log("error");
+  }finally{
+   loading.classList.add('d-none')
   }
 }
 
@@ -34,20 +46,23 @@ getGames("mmorpg");
 
 async function getDetails(id) {
 
-  console.log("id of the game is ", id);
   try {
+    loading.classList.remove('d-none')
     let response = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
     let data = await response.json();
     gameDetail = data
     const modal = new bootstrap.Modal(document.getElementById('fullscreenModal'));
-modal.show();
-showGameDetailsModal(gameDetail)
-    
+  modal.show();
+  ui.showGameDetailsModal(gameDetail)
+  loading.classList.add('d-none')
   } catch (error) {
-    console.log("error in details ", error);
+
+    console.log("error in details", error);
+  }finally{
+     loading.classList.add('d-none')
   }
 }
-
+window.getDetails = getDetails;
 const mylist = document.querySelector('ul')
 mylist.addEventListener('click', function(e){
   e.preventDefault();
@@ -58,64 +73,40 @@ getGames(clickedValue)
 })
 
 
-/*api return details */
+/*function displayGame() {
 
-var games = [];
+}*/
+//displayGame();
 
-function displayGame() {
-  var box = "";
-  for (var i = 0; i < games.length; i++) {
-    box += `
-    <div class="col-md-3 col-sm-6" >
-        <div class="gameCard my-2 overflow-hidden" onclick="getDetails(${games[i].id})">
-            <img class="card-img-top w-100 layer p-2" src="${games[i].thumbnail}" alt="Title" style="height:200px"/>
-            <div class="card-body bg-dark position-relative">
-            <div class="d-flex justify-content-between">
-            <h4 class="card-title text-white">${games[i].title}</h4>
-            <label class="badge badge-det d-flex justify-content-center align-items-center"  style="height:30px">Free</label> 
-            </div> 
-                <p class="text-center desc">${games[i].short_description.split(" ",8)}.</p>
-                    <div class="card-footer d-flex justify-content-between">
-                        <p class="badge bg-dark badge-font m-0">${games[i].genre}</p>
-                        <p class="badge bg-dark m-0">${games[i].platform}</p>
-                    </div>
-                </div>
-            </div>
-    </div>
-        `;
-  } gameData.innerHTML = box;
-
-}
-displayGame();
+ui.displayGame(games)
 
 
-
-
-const modalbody = document.querySelector('.modal-body')
-
-function showGameDetailsModal(gameDetail){
-  console.log("ana el gamedetail", gameDetail)
-  modalbody.innerHTML = ` 
-        <div class="container">
-          <div class="row text-white">
-          <div class="col-md-4">
-            <img src="${gameDetail.thumbnail}" alt="test">
-          </div>
-          <div class="col-md-8" id="details">
-            <h2>Title: ${gameDetail.title}</h2>
-            <h3>category: <span class ="badge bg-info text-dark m-1"> ${gameDetail.genre}</span></h3>
-            <h3>Platform:<span class ="badge bg-info text-dark m-1"> ${gameDetail.platform}</span></h3>
-            <h3>Status: <span class ="badge bg-info text-dark m-1 ">${gameDetail.status}</span></h3>
-            <p>${gameDetail.description}</p>
-            <a href="${gameDetail.game_url}" target="_blank" class="btn border border-warning text-white">Show Game</a>
-          </div>
-        </div>
-        </div>
-`
-}
 
 /*
-const exampleModalLabel = document.getElementById("exampleModalLabel")
-exampleModalLabel.innerHTML ="hamadaa"
+function showGameDetailsModal(gameDetail){
 
+}
 */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("navbarid");
+  if (!navbar) {
+    console.error("Navbar element not found!");
+    return;
+  }
+
+  const demoTop = navbar.offsetTop;
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= demoTop + 50) {
+      navbar.classList.add("fixed-top");
+      navbar.style.marginTop = "0px";
+      document.body.style.paddingTop = navbar.offsetHeight + "px";
+    } else {
+      navbar.classList.remove("fixed-top");
+      navbar.style.marginTop = "170px"; 
+      document.body.style.paddingTop = "0";
+    }
+  });
+});
--- ui.js
+++ ui.js
@@ -1,6 +1,54 @@
-displaydata()
 
+export class Ui {
+    constructor(gameDataContainer, modalBodyElement) {
+        this.gameData = gameDataContainer;
+        this.modalBody = modalBodyElement
+    }
+    displayGame(games) {
+        var box = "";
+        for (var i = 0; i < games.length; i++) {
+            box += `
+    <div class="col-md-3 col-sm-6" >
+        <div class="gameCard my-2 overflow-hidden" onclick="getDetails(${games[i].id})">
+            <img class="card-img-top w-100 layer p-2" src="${games[i].thumbnail}" alt="Title" style="height:200px"/>
+            <div class="card-body bg-dark position-relative">
+            <div class="d-flex justify-content-between">
+            <h4 class="card-title text-white">${games[i].title}</h4>
+            <label class="badge badge-det d-flex justify-content-center align-items-center"  style="height:30px">Free</label> 
+            </div> 
+                <p class="text-center desc">${games[i].short_description.split(" ", 8)}.</p>
+                    <div class="card-footer d-flex justify-content-between">
+                        <p class="badge bg-dark badge-font m-0">${games[i].genre}</p>
+                        <p class="badge bg-dark m-0">${games[i].platform}</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+        `;
+        } this.gameData.innerHTML = box;
 
-detailsData()
+    }
 
+    showGameDetailsModal(gameDetail) {
+       this.modalBody.innerHTML = ` 
+        <div class="container">
+          <div class="row text-white">
+          <div class="col-md-4">
+            <img src="${gameDetail.thumbnail}" alt="test">
+          </div>
+          <div class="col-md-8" id="details">
+            <h2>Title: ${gameDetail.title}</h2>
+            <h3>category: <span class ="badge bg-info text-dark m-1"> ${gameDetail.genre}</span></h3>
+            <h3>Platform:<span class ="badge bg-info text-dark m-1"> ${gameDetail.platform}</span></h3>
+            <h3>Status: <span class ="badge bg-info text-dark m-1 ">${gameDetail.status}</span></h3>
+            <p>${gameDetail.description}</p>
+            <a href="${gameDetail.game_url}" target="_blank" class="btn border border-warning text-white">Show Game</a>
+          </div>
+        </div>
+        </div>
+`
+    }
+}
+
+//display Details of the game
 
